8dd2e3166fac74e46f975a76b76b7e9cc3f92760
zjhe <hezijie@microsoft.com>
2023-10-24 22:13:47 +0800

    remove box from telemetry resource data

--- module_telemetry.tf
+++ module_telemetry.tf
@@ -1,6 +1,5 @@
 resource "modtm_telemetry" "this" {
-
-  tags = (/*<box>*/ (var.tracing_tags_enabled ? { for k, v in /*</box>*/ {
+  tags = {
     avm_git_commit           = "18a3c7014e381e065c9a5b22a3a577be32de4819"
     avm_git_file             = "module_telemetry.tf"
     avm_git_last_modified_at = "2023-10-24 07:58:31"
@@ -8,5 +7,5 @@ resource "modtm_telemetry" "this" {
     avm_git_repo             = "terraform-azure-mdc-defender-plans-azure"
     avm_yor_name             = "this"
     avm_yor_trace            = "e8fa1288-5384-4d8d-8c58-e297e99a6f39"
-  } /*<box>*/ : replace(k, "avm_", var.tracing_tags_prefix) => v } : {}) /*</box>*/)
+  }
 }
